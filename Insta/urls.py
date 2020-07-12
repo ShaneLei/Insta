0974246f6d12593ac9fba5a13f83dcf3308e74fa
shane <guolei1717@gmail.com>
2020-07-12 15:24:59 -0700

    Finish website development

--- Insta/urls.py
+++ Insta/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from Ins.views import SignUp
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Ins/', include('Ins.urls')),
+    path('', include('Ins.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('auth/signup', SignUp.as_view(), name='signup'),
 ]
